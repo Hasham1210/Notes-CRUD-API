b13cf2af56b2f13df21c72a7d8266cd7878c5011
unknown <hashamahmed41610t@gmail.com>
2026-08-24 02:33:15 +0500

    Add Vercel configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://notes-crud-backend.vercel.app/api",
+
 });
 
 export default api;
